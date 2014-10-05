9e7572f6eb2aad6b00fcf9e79c2785f7ba6ad8b7
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2014-10-05 13:29:21 +0200

    Use parent-require to load plugins

--- private/lib/core/loadPlugin.js
+++ private/lib/core/loadPlugin.js
@@ -4,13 +4,15 @@
  * @api public
  */
 
+var prequire = require('parent-require');
+
 function loadPlugin(plugin) {
 	console.log("[Load plugin] " + plugin);
 
 	var seminarjs = this;
 
 	// Load the plugin
-	var plugin = require("seminarjs-" + plugin);
+	var plugin = prequire("seminarjs-" + plugin);
 	plugin(seminarjs);
 }
 
